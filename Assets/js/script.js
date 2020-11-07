fa5152500e3e99a3cadbe2e3e7b70726c39cb7b7
kaylamuraoka <kmurs98@gmail.com>
2020-11-07 11:14:26 -1000

    added functions to the button in the script.js file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -21,23 +21,31 @@
 //     console.error(err);
 //   });
 
-const getRecipe = function(meal) {
+const getRecipe = function (meal) {
   let apiUrl = "https://recipe-puppy.p.rapidapi.com/?q=" + meal;
   let apiInfo = {
-    "method": "GET",
-    "headers": {
+    method: "GET",
+    headers: {
       "x-rapidapi-key": "a1763f65f3mshc9f6db6be3a5287p184f59jsneb238681d7af",
-      "x-rapidapi-host": "recipe-puppy.p.rapidapi.com"
-    }
-  }
+      "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+    },
+  };
   fetch(apiUrl, apiInfo)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data);
+          for (var i = 0; i < data.results.length; i++) {
+            var recipeName = data.results[i].title;
+            console.log(recipeName);
+            $("#options")
+              .append(`<button style=" width: 200px; height: 300px; padding: 10px; margin: 10px;" class="list-group-item text-left btn btn-outline-secondary" type="button" id="recipe_btn_0">
+         ${recipeName}
+        </button>`);
+          }
         });
       } else {
-        alert('Error: ' + response.statusText);
+        alert("Error: " + response.statusText);
       }
     })
     .catch((err) => {
@@ -50,7 +58,7 @@ const getRecipe = function(meal) {
 // Event listeners
 $("#search_button").click(function (event) {
   event.preventDefault();
-  let meal = $('#recipe_input').val();
-  console.log(meal)
+  let meal = $("#recipe_input").val();
+  console.log(meal);
   getRecipe(meal);
 });
