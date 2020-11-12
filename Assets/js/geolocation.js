ac4251c3bb6477c3b7949c03f902ea74fc5a347d
kaylamuraoka <kmurs98@gmail.com>
2020-11-12 12:08:58 -1000

    added markers to the google map

--- Assets/js/geolocation.js
+++ Assets/js/geolocation.js
@@ -1,20 +1,6 @@
 // // Using the Google Map Places API we can search for place information using a variety of categories
 // // DOCUMENTATION: https://developers.google.com/places/web-service/search
 
-// // Search for nearby Grocery Stores
-// //
-
-// // function createMarker(place) {
-// //   const marker = new google.maps.Marker({
-// //     map,
-// //     position: place.geometry.location,
-// //   });
-// //   google.maps.event.addListener(marker, "click", () => {
-// //     infowindow.setContent(place.name);
-// //     infowindow.open(map);
-// //   });
-// // }
-
 // Create the script tag, set the appropriate attributes
 // When the script is executed, it will call the function specified using the callback parameter.
 var script = document.createElement("script");
@@ -41,11 +27,11 @@ window.initMap = function () {
   const initialPosition = { lat: 37.7749, lng: -122.4194 };
 
   // Create a new map inside of the given HTML div with id="map"
-  const map = new google.maps.Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     // There are two required options for every map: center and zoom.
     // The map, centered at the initial position
     center: initialPosition,
-    zoom: 14,
+    zoom: 13,
   });
 
   // The marker, positioned at the user's current location
@@ -54,10 +40,16 @@ window.initMap = function () {
     position: initialPosition,
   });
 
+  // add event listener to the marker so that when clicked on an infowindow pops up
+  myLocationMarker.addListener("click", function () {
+    var infowindow = new google.maps.InfoWindow();
+    infowindow.open(map, myLocationMarker);
+    infowindow.setContent("Your Current Location");
+  });
+
   // Get the users current location
   if (navigator.geolocation) {
     // Browser supports Geolocation
-
     navigator.geolocation.getCurrentPosition(function (position) {
       console.log(
         `Current Location: Latitude = ${position.coords.latitude}, Longitude = ${position.coords.longitude}`
@@ -74,6 +66,10 @@ window.initMap = function () {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       });
+
+      // once the map is loaded to the users location the loading symbol dissappears
+      $("#loading").addClass("hide");
+
       const url =
         "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
         position.coords.latitude +
@@ -81,8 +77,73 @@ window.initMap = function () {
         position.coords.longitude +
         "&radius=5000&type=grocery_or_supermarket&keyword=grocery&key=" +
         apiKey;
-      getGroceryStores(url);
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+      // fetch the nearby grocery stores
+      fetch(proxyurl + url).then(function (response) {
+        if (!response.ok) {
+          console.log("Looks like there was a problem. " + response.statusText);
+          return;
+        }
+        // Examine the text in the response
+        response
+          .json()
+          .then(function (data) {
+            console.log(data);
+            var stores_array = [];
+            var store = {};
+            for (var i = 0; i < data.results.length; i++) {
+              const contentString =
+                '<h3 id="firstHeading" class="firstHeading">' +
+                data.results[i].name +
+                "</h3>" +
+                '<div id="bodyContent">' +
+                "<p><b>Address: </b>" +
+                data.results[i].vicinity +
+                "</br>" +
+                "<b>Distance: </b>";
+              const infowindow = new google.maps.InfoWindow({
+                content: contentString,
+              });
+
+              const marker = new google.maps.Marker({
+                position: data.results[i].geometry.location,
+                map: map,
+                icon: {
+                  url: data.results[i].icon,
+                  scaledSize: new google.maps.Size(40, 40),
+                },
+                title: data.results[i].name,
+              });
+
+              marker.addListener("click", function () {
+                {
+                  infowindow.setContent(contentString);
+                  infowindow.open(map, marker);
+                }
+                setTimeout(function () {
+                  infowindow.close();
+                }, 5000);
+              });
+
+              store = {
+                name: data.results[i].name,
+                is_open: data.results[i].opening_hours.open_now,
+                address: data.results[i].vicinity,
+                lat: data.results[i].geometry.location.lat,
+                lat: data.results[i].geometry.location.lng,
+              };
+              stores_array.push(store);
+            }
+            console.log(stores_array);
+            displayStores(stores_array);
+          })
+          .catch(function (err) {
+            console.error("Fetch Error: ", err);
+          });
+      });
     }),
+      // if there is an error
       function (error) {
         // Browser doesn't support Geolocation
         alert(`Error (${error.code}): ${error.message}`);
@@ -94,57 +155,17 @@ window.initMap = function () {
 // Append the 'script' element to 'head'
 document.head.appendChild(script);
 
-function getGroceryStores(url) {
-  const proxyurl = "https://cors-anywhere.herokuapp.com/";
-  fetch(proxyurl + url).then(function (response) {
-    if (!response.ok) {
-      console.log("Looks like there was a problem. " + response.statusText);
-      return;
-    }
-    // Examine the text in the response
-    response
-      .json()
-      .then(function (data) {
-        console.log(data);
-        var stores_array = [];
-        var store = {};
-        for (var i = 0; i < data.results.length; i++) {
-          store = {
-            name: data.results[i].name,
-
-            open_hours: data.results[i].opening_hours,
-            address: data.results[i].vicinity,
-            latlng: data.results[i].geometry.location,
-          };
-          stores_array.push(store);
-        }
-        console.log(stores_array);
-
-        // print store list to the screen
-        for (var i = 0; i < stores_array.length; i++) {
-          $("#store_list").append(
-            `<div id="name">${stores_array[i].name} , Is your store open?: ${stores_array[i].open_hours.open_now}, address: ${stores_array[i].address}<\div>`
-          );
-        }
-      })
-      .catch(function (err) {
-        console.error("Fetch Error: ", err);
-      });
-  });
-}
-
-// create markers at the grocery store locations
-function createMarker(place) {
-  const infowindow = new google.maps.InfoWindow();
-  for (var i = 0; i < stores_array.length; i++) {
-    const marker = new google.maps.Marker({
-      map,
-      position: place.latlng,
-      title: place.name,
-    });
-    google.maps.event.addListener(marker, "click", function () {
-      infowindow.setContent(place.name);
-      infowindow.open(map);
-    });
+var displayStores = function (resultArray) {
+  // print store list to the screen
+  if (resultArray.length === 0) {
+    $("#store_list").append(
+      `<p>There is no grocery stores within a 5,000 meter radius of your location</p>`
+    );
+    return;
   }
-}
+  for (var i = 0; i < resultArray.length; i++) {
+    $("#store_list").append(
+      `<li id="name">${resultArray[i].name} , Is your store open?: ${resultArray[i].is_open}, address: ${resultArray[i].address}<\div>`
+    );
+  }
+};
